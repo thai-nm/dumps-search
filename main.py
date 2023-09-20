afe590b6e649ac0cdcc68d696e700002527b334b
Thai Nguyen <thainm.uet@gmail.com>
2023-09-20 18:20:39 +0700

    Support generating PDF files with a page range

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import weasyprint
 import argparse
+import PyPDF2
 
-from parser import parser
+from parser import prepare_parser
 from googlesearch import search
 
 
@@ -20,26 +21,27 @@ def save_question_pages_as_pdf(question_index, pages):
     pdf_filename = f"question-{question_index}.pdf"
 
     try:
-        pdf = weasyprint.HTML(answer_url).write_pdf()
-        pages_to_keep = pages
+        weasyprint.HTML(answer_url).write_pdf(pdf_filename)
 
-        output_pdf = weasyprint.PDFFile()
-        for page_num in pages_to_keep:
-            if page_num <= len(pdf.pages):
-                output_pdf.add_page(pdf.pages[page_num - 1])
+        pdf = PyPDF2.PdfReader(pdf_filename)
+        pdf_writer = PyPDF2.PdfWriter()
+
+        for page_num in pages:
+            if 1 <= page_num <= len(pdf.pages):
+                pdf_writer.add_page(pdf.pages[page_num - 1])
 
         with open(pdf_filename, "wb") as output_file:
-            output_pdf.write(output_file)
+            pdf_writer.write(output_file)
 
         print(
-            f"Question {question_index} - Pages {', '.join(map(str, pages_to_keep))} saved as '{pdf_filename}'"
+            f"Question {question_index} - Pages {', '.join(map(str, pages))} saved as '{pdf_filename}'"
         )
     except Exception as e:
         print(f"Error saving question {question_index} as PDF: {e}")
 
 
 if __name__ == "__main__":
-    options = parser.parse_args()
+    options = prepare_parser().parse_args()
 
     for question_index in range(options.start, options.end + 1):
         save_question_pages_as_pdf(question_index, pages=options.pages)
