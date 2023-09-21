e9f19725aced44cdf97083ca6d3dc33ff5da5abb
Thai Nguyen <thainm.uet@gmail.com>
2023-09-22 00:26:23 +0700

    Update log message

--- parser.py
+++ parser.py
@@ -12,6 +12,9 @@ def prepare_parser():
         "--end", default=190, type=int, help="Last question index to query"
     )
     parser.add_argument(
-        "--pages", default=[3, 5], type=int, nargs="+", help="Specify pages to generate"
+        "--pages", default=[3,4,5], type=int, nargs="+", help="Specify pages to generate"
+    )
+    parser.add_argument(
+        "--exam", default="gcp-ace", choices=["gcp-ace", "aws-scs"], help="Exam name"
     )
     return parser
